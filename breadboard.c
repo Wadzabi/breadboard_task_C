01fda876ecb0351d701475e92d16d4d18d4e9ec1
Wadzabi <martin.piqueras1@gmail.com>
2021-10-31 22:39:13 +0100

    first version of add_resistor

--- breadboard.c
+++ breadboard.c
@@ -3,21 +3,54 @@
 #include "breadboard.h"
 
 
-int add_resistor(resistor_node *head, int col1, int col2, int row){
-    printf("Enter row ");
+int add_resistor(resistor_node *head, int c1, int c2, int r1){
+    coordinate point = {c1, r1};
+    for(;point.x<=c2; point.x++){
+        if(is_occupied(head, point)) return -1;
+    }
+    resistor_node *new_node = malloc(sizeof(resistor_node));
+    new_node->col1 = c1;
+    new_node->col2 = c2;
+    new_node->row = r1;
 
     if (head == NULL){
-        head = malloc(sizeof(resistor_node));
+        new_node->next = NULL;
+        new_node->previous = NULL;
+        head = new_node;
+        
+        return 0;
     }
+
+    if (new_node->row < head->row || (new_node->col1 < head->col1 && new_node->row == head->row)){
+        new_node->next = head;
+        new_node->previous = NULL;
+        head->previous = new_node;
+        head = new_node;
+        return 0;
+
+    }
+    resistor_node *current = head;
+    while (current->next != NULL){
+        if (new_node->row < current->next->row || (new_node->col1 < current->next->col1 && new_node->row == current->next->row)){
+            break;
+        }
+        current = current->next;   
+    }
+    new_node->next = current->next;
+    new_node->previous = current;
+    if(current->next != NULL) current->next->previous = new_node;
+    current->next = new_node;
+    return 0;
+
+ 
+
 
 }
 
 bool is_occupied(resistor_node *head, coordinate point){
     resistor_node *current = head;
     while (current != NULL){
-        if (point.x >= current->col1 && point.x <= current->col2 && point.y == current->row ){
-            return true;
-        }
+        if (point.x >= current->col1 && point.x <= current->col2 && point.y == current->row ) return true;
         current = current->next;
     }
     return false;
