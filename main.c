91cb324146119b31e66060f0578ac581bfa63fde
Wadzabi <martin.piqueras1@gmail.com>
2021-11-08 11:02:19 +0100

    print, add and remove usable with menu.

--- main.c
+++ main.c
@@ -1,9 +1,48 @@
 #include "breadboard.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
+#define BETWEEN(value, max, min) (value <= max && value >= min)
+
 
 enum menu_options{PRINT_BOARD = 1, ADD_RES, REMOVE_RES, CHECK_CONNECTION, EXIT};
+
+int add_R_to_board(breadboard board, resistor_node **resistors){
+    int x1, x2, y;
+    printf("\nEnter x and y coordinates of the point where you want to place a resistor.\nUse x, y format): ");
+    scanf("%d, %d", &x1, &y);
+    printf("\nEnter second x coordinate for resistor: ");
+    scanf("%d", &x2);
+
+    printf("test print x1 = %d   x2 = %d   y = %d", x1, x2, y);
+    printf("rows and column values, %d and %d", board.rows, board.columns);
+
+    if (!(BETWEEN(x1, board.columns-1, 0) && BETWEEN(x2, board.columns-1, 0) && BETWEEN(y, board.rows-1, 0))) {
+        printf("\nOne or more of the points was not within the bounds of the breadboard.\nReturning to menu\n");
+        return -1;
+    }
+
+    if(x1 == x2){
+        printf("\nTried to place both resistor legs on same point. Not possible.");
+        return -1;
+    }
+    if(add_resistor(resistors, x1, x2, y) == -1){
+        printf("\nResistor could not be added, place is occupied");
+        return -1;
+    }
+    return 0;
+    
+
+}
+
+void del_R_from_board(resistor_node **resistors){
+    coordinate point;
+    printf("Enter coordinates of resistor you want to remove.\nUse format (x, y): ");
+    scanf("%d, %d", &point.x, &point.y);
+    remove_resistor(resistors, point);
+    return;
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -25,19 +64,22 @@ int main(int argc, char const *argv[])
     int menu_option;
     
     while(true){
-        printf("1. Print board\n2. Add resistor\n3. Remove resistor\n4. Check connection\n5. Exit\n");
+        printf("\n1. Print board\n2. Add resistor\n3. Remove resistor\n4. Check connection\n5. Exit\n");
         printf("Please choose an option: ");
         scanf("%d", &menu_option);
 
         switch(menu_option){
             case PRINT_BOARD:
-                printf("board will be printed");
+                printf("board will be printed\n");
+                print_board(resistors, board);
                 break;
             case ADD_RES:
-                printf("resistor will be added");
+                printf("\nresistor will be added");
+                add_R_to_board(board, &resistors);
                 break;
             case REMOVE_RES:
-                printf("resistor will be removed");
+                printf("\nresistor will be removed");
+                del_R_from_board(&resistors);
                 break;
             case CHECK_CONNECTION:
                 printf("connection will be checked");
